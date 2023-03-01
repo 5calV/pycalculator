ce9d1937c993f74dcfe3e88a5fb2933a5dc3e2c9
5calV <110572069+5calV@users.noreply.github.com>
2023-03-01 12:21:22 +0100

    Fixed "ZeroDivide" Error.

---
+++ calculator.py
@@ -0,0 +1,68 @@
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+#   ╭∩╮   ╭∩╮
+#    \(͡⚈ᴗ͡⚈)/
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+# Loop Start
+while True:
+
+
+# Global Variables
+
+# Addition, Subtraction, Multiplication or Division?
+    calculation = input("Which Type of Calculation do you want to use?\nAddition ( 1 )\nSubtraction ( 2 )\nMultiplication ( 3 )\nDivision ( 4 )\n")
+
+# User gives number 1, number 2.
+    a = float(input('Enter Number 1: '))
+    b = float(input('Enter Number 2: '))
+
+# Result
+    addition = (a+b)
+    subtraction = (a-b)
+    multiplication = (a*b)
+#    division = (a/b)
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+# Addition
+
+    if (calculation) == "1":
+        print (('Your Result:'), addition)
+
+# Subtraction
+
+    if (calculation) == "2":
+        print (('Your Result:'), subtraction)
+
+# Multiplication
+
+    if (calculation) == "3":
+        print (('Your Result:'), multiplication)
+
+
+
+# Division
+
+    if calculation == "4":
+        try:
+            division = a / b
+            print("Your Result:", division)
+        except ZeroDivisionError:
+            print("Numbers can not be zero!")
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------
+
+# Restart?
+
+    restart = input("Would you like to do another Calculation? ( y / n )\n")
+
+    if (restart) == ("y"):
+        continue
+    else:
+        break
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
